f43679e7e8dc42248e5a6acaa6b318cfa0619ed8
SerpentSnek <ykkwong@ucdavis.edu>
2023-06-19 23:31:44 -0700

    i hate it here

---
+++ weather-app/src/App.js
@@ -0,0 +1,18 @@
+// search react developer tools for extension that shows component hierarchy
+import React from "react";
+// import { useState } from 'react';
+// import Alert from './components/Alert';
+// import Button from './components/Button';
+import "./App.css";
+//https://stackoverflow.com/questions/41292559/could-not-find-a-declaration-file-for-module-module-name-path-to-module-nam#42505940
+// import * as yourModuleName from 'module-name';
+// const yourModuleName = require('module-name');
+// const Unicons = require('@iconscout/react-unicons');
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bggradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
+  );
+}
+
+export default App;
