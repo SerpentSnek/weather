7df501e88475b4410053b3e999e804f6fb7d6882
SerpentSnek <ykkwong@ucdavis.edu>
2023-06-20 12:22:41 -0700

    dependency hell gone lessgo

--- weather-app/src/pages/_app.js
+++ weather-app/src/pages/_app.js
@@ -3,3 +3,12 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+// import React from 'react'
+
+// export const _app = () => {
+//   return (
+//     <div className='Home'>
+//       <h1 className='mx-auto bg-gradient-to-br from-cyan-400 to-red-800'>hello</h1>
+//     </div>
+//   )
+// }
