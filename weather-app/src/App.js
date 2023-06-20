7bf02909d26203fd18fa912c8671f9333ee88c5b
SerpentSnek <ykkwong@ucdavis.edu>
2023-06-20 00:51:59 -0700

    god why

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -11,7 +11,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bggradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
+    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 from-cyan-700 to-blue-700`}>
+    </div>
   );
 }
 
